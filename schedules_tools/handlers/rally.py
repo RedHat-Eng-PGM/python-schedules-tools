3d4cb5530ee6d1fe8b9cb034147b9f5083c71189
Martin Pavlasek <mpavlase@redhat.com>
2017-01-27 12:07:13 +0100

    rally handler: fix import, user rally_iter as schedule.name

--- schedules_tools/handlers/rally.py
+++ schedules_tools/handlers/rally.py
@@ -20,6 +20,7 @@ class ScheduleHandler_rally(ScheduleHandlerBase):
 
     # Schedule
     def import_schedule(self, rally_cfg_file):
+        self.schedule = models.Schedule()
         start_time = None
 
         options = ['--config=%s' % rally_cfg_file]
@@ -27,10 +28,11 @@ class ScheduleHandler_rally(ScheduleHandlerBase):
 
         rally = Rally(server, user, password, workspace=workspace, project=project)
 
-        query_criteria = 'Iteration.Name = "%s"' % self.rally_iter
+        rally_iter = self.opt_args['rally_iter']
+        query_criteria = 'Iteration.Name = "%s"' % rally_iter
 
         response = rally.get('Iteration', fetch=True,
-                             query='Name = "%s"' % self.rally_iter)
+                             query='Name = "%s"' % rally_iter)
         if response.errors:
             sys.stdout.write("\n".join(response.errors))
             sys.exit(1)
@@ -52,11 +54,11 @@ class ScheduleHandler_rally(ScheduleHandlerBase):
         if not start_time:
             start_time = datetime.datetime.combine(datetime.date.today(), datetime.time(8))
         max_end_time = start_time
-        self.dStart = start_time
+        self.schedule.dStart = start_time
 
         for story in response:
             print story.Name
-            t = models.Task(schedule=self, level=1)
+            t = models.Task(self.schedule, level=1)
             t.index = index
             index += 1
             t.name = story.Name
@@ -66,7 +68,7 @@ class ScheduleHandler_rally(ScheduleHandlerBase):
             story.Tasks.sort(key=lambda x: x.TaskIndex)
             for task in story.Tasks:
                 print '-- %s  |  %sh  |  %s' % (task.Name, task.Estimate, task.Owner.Name)
-                t_in = models.Task(schedule=self, level=2)
+                t_in = models.Task(self.schedule, level=2)
                 t_in.index = index
                 index += 1
                 t_in.name = task.Name
@@ -76,13 +78,13 @@ class ScheduleHandler_rally(ScheduleHandlerBase):
 
                 # look for resource
                 resource_id = None
-                for r_id, resource in self.resources.items():
+                for r_id, resource in self.schedule.resources.items():
                     if resource == task.Owner.Name:
                         resource_id = r_id
                         break
                 if not resource_id:
-                    resource_id = len(self.resources) + 1
-                    self.resources[resource_id] = str(task.Owner.Name)
+                    resource_id = len(self.schedule.resources) + 1
+                    self.schedule.resources[resource_id] = str(task.Owner.Name)
 
                 t_in.resource = resource_id
 
@@ -90,9 +92,9 @@ class ScheduleHandler_rally(ScheduleHandlerBase):
             print ''
             t.dFinish = max_st_end_time
             max_end_time = max(max_end_time, t.dFinish)
-            self.tasks.append(t)
+            self.schedule.tasks.append(t)
 
-        self.dFinish = max_end_time
+        self.schedule.dFinish = max_end_time
 
     # Schedule
     def export_schedule(self, out_file):
