1f26ebdbac6ec3fe4d7884d65c6e6062595372b4
Martin Pavlasek <mpavlase@redhat.com>
2017-01-31 10:06:50 +0100

    converter: Add --handlers-path option to extend discovery paths
    Can be used multiple times, if there is discovered handler with same
    name, old one will be overriden.

--- schedules_tools/schedule_converter.py
+++ schedules_tools/schedule_converter.py
@@ -8,6 +8,7 @@ import logging
 
 
 logger = logging.getLogger('converter')
+handler_class_template = re.compile('^ScheduleHandler_(\S+)$')
 VALID_MODULE_NAME = re.compile(r'[_a-z]\w*\.py$', re.IGNORECASE)
 BASE_DIR = os.path.dirname(os.path.realpath(
     os.path.join(__file__, os.pardir)))
@@ -41,7 +42,8 @@ class HandleWithoutExport(Exception):
 
 
 class AutodiscoverHandlers(object):
-    _top_level_dir = os.path.join(BASE_DIR, PARENT_DIRNAME)
+    _top_level_dir = os.path.join(BASE_DIR)
+    _discovered_handlers = dict()
 
     def _get_name_from_path(self, path):
         path = os.path.splitext(os.path.normpath(path))[0]
@@ -64,45 +66,61 @@ class AutodiscoverHandlers(object):
 
         for name in dir(module):
             obj = getattr(module, name)
-            if isinstance(obj, type) and issubclass(obj, handlers.ScheduleHandlerBase):
-                pieces = obj.__name__.split('_')
-                if len(pieces) < 2:
-                    continue
+            is_inst = isinstance(obj, type)
 
-                val = dict()
-                key = pieces[-1]
-                val['class'] = obj
-                val['provide_export'] = obj.provide_export
+            if not is_inst:
+                continue
 
-                ret[key] = val
+            if obj == handlers.ScheduleHandlerBase:
+                continue
+
+            # TODO(mpavlase): figure out more reliable way to test subclasses
+            #is_subclass = issubclass(obj, handlers.ScheduleHandlerBase)
+            #if not is_subclass:
+            #    continue
+
+            key = re.findall(handler_class_template, obj.__name__)
+            if not key:
+                continue
+
+            key = key[0]
+            val = dict()
+            val['class'] = obj
+            val['provide_export'] = obj.provide_export
+
+            ret[key] = val
+            logger.debug('Discovered new handler: {} from {}'.format(key, module))
         return ret
+
+    def _discover_path(self, path, start_dir):
+        full_path = os.path.join(start_dir, path)
+
+        if os.path.isfile(full_path):
+            if not VALID_MODULE_NAME.match(path):
+                # valid Python identifiers only
+                return
+
+            name = self._get_name_from_path(full_path)
+            module = self._get_module_from_name(name)
+            classes = self._find_classes(module)
+
+            for k in classes.keys():
+                if k in self._discovered_handlers.keys():
+                    msg = ('Found handler with same name (would be '
+                           'overrriden): {} ({}, {})').format(
+                        k, self._discovered_handlers[k], classes[k])
+                    logger.warning(msg)
+
+            self._discovered_handlers.update(classes)
 
     def discover(self, start_dir):
         paths = os.listdir(start_dir)
-        ret = {}
+        self._discovered_handlers = dict()
 
         for path in paths:
-            full_path = os.path.join(start_dir, path)
-            
-            if os.path.isfile(full_path):
-                if not VALID_MODULE_NAME.match(path):
-                    # valid Python identifiers only
-                    continue
-                
-                name = self._get_name_from_path(full_path)
-                module = self._get_module_from_name(name)
-                classes = self._find_classes(module)
-            
-                for k in classes.keys():
-                    if k in ret.keys():
-                        msg = ('Found duplicated handler name (would be '
-                               'overrriden): {} ({}, {})').format(
-                            k, ret[k], classes[k])
-                        logger.warning(msg)
+            self._discover_path(path, start_dir)
 
-                ret.update(classes)
-        
-        return ret
+        return self._discovered_handlers
 
 
 class ScheduleConverter(object):
@@ -112,10 +130,24 @@ class ScheduleConverter(object):
     schedule = None
 
     def __init__(self):
-        ad = AutodiscoverHandlers()
         #handlers_path = os.path.join(BASE_DIR, self.handlers_dir)
         handlers_path = os.path.join(BASE_DIR, PARENT_DIRNAME, self.handlers_dir)
-        self.handlers = ad.discover(handlers_path)
+        self.add_discover_path(handlers_path)
+
+    def add_discover_path(self, handlers_path):
+        ad = AutodiscoverHandlers()
+        new_handlers = ad.discover(handlers_path)
+
+        # notify about overriden handlers
+        keys_existing = set(self.handlers.keys())
+        keys_new = set(new_handlers.keys())
+        keys_diff = keys_existing.intersection(keys_new)
+        if keys_diff:
+            keys_str = list(keys_diff)
+            logger.info('Overriding handlers: {} (from {})'.format(
+                keys_str, handlers_path))
+
+        self.handlers.update(new_handlers)
 
         for key, val in self.handlers.iteritems():
             if val['provide_export']:
@@ -134,6 +166,8 @@ class ScheduleConverter(object):
         handle_class = self.find_handle(handle)['class']
         handle_inst = handle_class(opt_args=handler_opt_args)
         sch = handle_inst.import_schedule(handle)
+        assert sch is not None, 'Import handle {} didn\'t return filled ' \
+                                'schedule!'.format(handle_class)
         self.schedule = sch
         return self.schedule
 
@@ -151,7 +185,6 @@ class ScheduleConverter(object):
         v_major = handler_opt_args['major']
         v_minor = handler_opt_args['minor']
         v_maint = handler_opt_args['maint']
-
         handle_class = self._get_export_handle_cls(target_format)
         handle_inst = handle_class(schedule=self.schedule,
                                    opt_args=handler_opt_args)
@@ -176,6 +209,11 @@ if __name__ == '__main__':
                         default='')
     parser.add_argument('--rally-iter', help='Rally iteration to import',
                         default='')
+    parser.add_argument('--handlers-path',
+                        help='Add path to discover handlers (can be called '
+                             'several times)',
+                        action='append',
+                        default=[])
 
     parser.add_argument('source', type=str,
                         help='Source of schedule (file/URL/...)')
@@ -188,8 +226,11 @@ if __name__ == '__main__':
 
     arguments = parser.parse_args()
     opt_args = vars(arguments)
-    converter.import_schedule(arguments.source, handler_opt_args=opt_args)
 
+    for path in opt_args.pop('handlers_path'):
+        converter.add_discover_path(path)
+
+    converter.import_schedule(arguments.source, handler_opt_args=opt_args)
     converter.export_handle(arguments.target_format,
                             arguments.out_file,
                             handler_opt_args=opt_args)
