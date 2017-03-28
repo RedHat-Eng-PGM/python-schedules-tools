00ee1ced446836c8a7f4ebd000acf6ac6e94f9ee
Martin Pavlasek <mpavlase@redhat.com>
2017-03-28 12:51:25 +0200

    update test_handlers to use 'local' storage handler + updates on testrunner

--- schedules_tools/tests/test_handlers.py
+++ schedules_tools/tests/test_handlers.py
@@ -1,18 +1,24 @@
 from schedules_tools import testrunner
 from schedules_tools import discovery
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 discovery.schedule_handlers.run_discovery()
+discovery.storage_handlers.run_discovery()
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
 
 def pytest_generate_tests(metafunc):
     idlist = []
     argvalues = []
-    for scenario in metafunc.cls.combinations:
-        idlist.append(scenario[0])
-        items = scenario[1].items()
-        argnames = [x[0] for x in items]
-        argvalues.append(([x[1] for x in items]))
+    for scenario in metafunc.cls.test_suite:
+        for combination in scenario['combinations']:
+            idlist.append(combination[0])
+            items = combination[1].items()
+            argnames = ['basedir'] + [x[0] for x in items]
+            argvalues.append([scenario['basedir']] + [x[1] for x in items])
     metafunc.parametrize(argnames, argvalues, ids=idlist, scope="class")
 
 IMPORT = 'import'
@@ -20,46 +26,55 @@ EXPORT = 'export'
 
 
 class TestHandlers(object):
-    combinations = [
-        ('tjx-in', {'handler': 'tjx',
-                    'reference': 'schedules_tools/tests/ref.json',
-                    'testfile': 'schedules_tools/tests/data/input.tjx',
-                    'action': IMPORT}),
-        ('tjx-out', {'handler': 'tjx',
-                     'reference': 'schedules_tools/tests/ref.json',
-                     'testfile': 'schedules_tools/tests/data/output.tjx',
-                     'action': EXPORT}),
-        ('msp-in', {'handler': 'msp',
-                    'reference': 'schedules_tools/tests/ref.json',
-                    'testfile': 'schedules_tools/tests/data/input.xml',
-                    'action': IMPORT}),
-        ('msp-out', {'handler': 'msp',
-                     'reference': 'schedules_tools/tests/ref.json',
-                     'testfile': 'schedules_tools/tests/data/output.xml',
-                     'action': EXPORT}),
-        ('tjx2-in', {'handler': 'tjx2',
-                     'reference': 'schedules_tools/tests/ref.json',
-                     'testfile': 'schedules_tools/tests/data/input-v2.tjx',
-                     'action': IMPORT}),
-        ('html-out', {'handler': 'html',
-                      'reference': 'schedules_tools/tests/ref.json',
-                      'testfile': 'schedules_tools/tests/data/output.html',
-                      'action': EXPORT}),
-        ('json-out', {'handler': 'json',
-                      'reference': 'schedules_tools/tests/ref.json',
-                      'testfile': 'schedules_tools/tests/data/output-struct.json',
-                      'action': EXPORT}),
-        ('jsonflat-out', {'handler': 'jsonflat',
-                          'reference': 'schedules_tools/tests/ref.json',
-                          'testfile': 'schedules_tools/tests/data/output-flat.json',
+    test_suite = [
+        {'basedir': BASE_DIR,
+         'combinations': [
+            ('tjx-in', {'handler': 'tjx',
+                        'reference': 'ref.json',
+                        'testfile': 'data/input.tjx',
+                        'action': IMPORT}),
+            ('tjx-out', {'handler': 'tjx',
+                         'reference': 'ref.json',
+                         'testfile': 'data/output.tjx',
+                         'action': EXPORT}),
+            ('msp-in', {'handler': 'msp',
+                        'reference': 'ref.json',
+                        'testfile': 'data/input.xml',
+                        'action': IMPORT}),
+            ('msp-out', {'handler': 'msp',
+                         'reference': 'ref.json',
+                         'testfile': 'data/output.xml',
+                         'action': EXPORT}),
+            ('tjx2-in', {'handler': 'tjx2',
+                         'reference': 'ref.json',
+                         'testfile': 'data/input-v2.tjx',
+                         'action': IMPORT}),
+            ('html-out', {'handler': 'html',
+                          'reference': 'ref.json',
+                          'testfile': 'data/output.html',
                           'action': EXPORT}),
+            ('json-out', {'handler': 'json',
+                          'reference': 'ref.json',
+                          'testfile': 'data/output-struct.json',
+                          'action': EXPORT}),
+            ('jsonflat-out', {'handler': 'jsonflat',
+                              'reference': 'ref.json',
+                              'testfile': 'data/output-flat.json',
+                              'action': EXPORT}),
+            ]
+         }
     ]
 
-    def test_handler(self, handler, reference, testfile, action):
-        runner = testrunner.TestRunner(handler, reference)
+    def test_handler(self, handler, basedir, reference, testfile, action):
+        testfile_abspath = os.path.join(basedir, testfile)
+        reffile_abspath = os.path.join(basedir, reference)
+        opt_args = {
+            'source_storage_format': 'local'
+        }
+        runner = testrunner.TestRunner(handler, reffile_abspath, opt_args=opt_args)
         if action == EXPORT:
-            runner.test_output(testfile)
+            runner.test_output(testfile_abspath)
         elif action == IMPORT:
-            runner.test_input(testfile)
+            runner.test_input(testfile_abspath)
         else:
             logger.warn('Unknown action to test: {}'.format(action))
