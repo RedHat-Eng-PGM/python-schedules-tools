e901ede146fa57c16b5c0c63f7b6c9f8572beec4
Martin Pavlasek <mpavlase@redhat.com>
2017-11-20 15:37:47 +0100

    testrunner: added comments to TestRunner class/methods

--- schedules_tools/testrunner.py
+++ schedules_tools/testrunner.py
@@ -7,6 +7,13 @@ from schedules_tools import models
 
 
 class TestRunner(object):
+    """
+    This class handles testing schedule import and export of schedules.
+
+    If some test comparison fails (expected content is different of actual),
+    there is a feature that stores input/output content into directory to help
+    debugging (test_failures_output_dir).
+    """
     handler_name = None
     options = None
     json_reference_file = None
@@ -17,8 +24,14 @@ class TestRunner(object):
                  test_id=None, test_failures_output_dir=None):
         """
         Args:
-            handler_name: name as str, such as 'tjx'
-            json_reference_file: reference to make comparison
+            handler_name: 'tjx'|'tjx2'|'msp'|'jsonstruct'|...
+            json_reference_file: filename of JSON dump that contains correct
+                (=reference) test data to compare with actual
+            options: dict of another options those are passed to schedule_import
+            test_id: string label of actual test, it's used for naming
+                debug files, if some test fails
+            test_failures_output_dir: path to directory where would be stored
+                test data of failed tests
         """
         self.handler_name = handler_name
         self.options = options
@@ -27,6 +40,14 @@ class TestRunner(object):
         self.test_failures_output_dir = test_failures_output_dir
 
     def make_json_reference(self, input_file):
+        """
+        Takes input file and produce JSON dump and store it into
+        json_reference_file (constructor of class).
+
+        Args:
+            input_file: source of data to produce reference json representation
+                        for further comparison in import/export tests.
+        """
         conv = converter.ScheduleConverter()
         conv.import_schedule(input_file,
                              schedule_src_format=self.handler_name)
@@ -51,6 +72,15 @@ class TestRunner(object):
         return self._dict_to_string(json_loaded)
 
     def _dump_output_as_file(self, reference, test_output):
+        """
+        Store test data into files to help debugging tests (differences
+        between conversions).
+        Path of these files is in 'test_failures_output_dir' variable.
+
+        Args:
+            reference: reference (=correct) result of schedule conversion
+            test_output: actual result of schedule conversion
+        """
         if not (self.test_failures_output_dir and
                 self.test_id):
             return
@@ -71,6 +101,18 @@ class TestRunner(object):
                               separators=(',', ': '))
 
     def test_input(self, input_file):
+        """
+        Testing import:
+        Takes handle (input_file) to import source data of schedule (result is
+        Schedule object), produces JSON dump and compare that with reference dump
+        (json_reference_file).
+
+        Args:
+            input_file:
+
+        Returns:
+
+        """
         reference_str = self._load_reference_as_json_str()
         conv = converter.ScheduleConverter()
         conv.import_schedule(input_file,
@@ -87,6 +129,21 @@ class TestRunner(object):
         assert input_str == reference_str
 
     def test_output(self, output_file, patch_output=None):
+        """
+        Testing export:
+        Takes reference JSON dump to fill Schedule object and pass it into Converter
+        to produce exported output (a file). This exported file is finally compared
+        with already stored output (output_file)
+
+        Optionally
+
+        Args:
+            output_file:
+            patch_output: function (callable object) to alter test content before comparing.
+
+        Returns:
+
+        """
         reference_dict = self._load_reference_as_dict()
         reference_schedule = models.Schedule.load_from_dict(reference_dict)
         _, temp_reference_file = tempfile.mkstemp()
