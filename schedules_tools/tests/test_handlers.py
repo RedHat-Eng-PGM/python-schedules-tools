5baf2e9c3d8fb73fa2cafbed5e4976dfdaa87ad5
Martin Pavlasek <mpavlase@redhat.com>
2017-12-06 14:49:24 +0100

    tests: fixed regeneration of schedule data

--- schedules_tools/tests/test_handlers.py
+++ schedules_tools/tests/test_handlers.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import shutil
 
 from schedules_tools.converter import ScheduleConverter
 from schedules_tools.tests import jsondate
@@ -76,7 +77,6 @@ class TestHandlers(object):
         return data
 
     def test_import(self, handler_name, import_schedule_file):
-        regenerate = os.environ.get('REGENERATE', False) == 'true'
         converter_options = {
             'source_storage_format': 'local'
         }
@@ -92,9 +92,11 @@ class TestHandlers(object):
         imported_schedule_dict = schedule.dump_as_dict()
         self._clean_interm_struct(imported_schedule_dict)
 
-        interm_reference_file = os.path.join(BASE_DIR, self.intermediary_reference_file)
+        interm_reference_file = os.path.join(BASE_DIR,
+                                             self.intermediary_reference_file)
+        regenerate = os.environ.get('REGENERATE', False) == 'true'
         if regenerate:
-            logger.info('test_import: Regenerating interm. reference file')
+            logger.info('test_import: Regenerating interm. reference file from imported schedule.')
 
             with open(interm_reference_file, 'w+') as fd:
                 jsondate.dump(imported_schedule_dict,
@@ -110,17 +112,29 @@ class TestHandlers(object):
         assert reference_dict == imported_schedule_dict
 
     def test_export(self, handler_name, export_schedule_file, tmpdir):
-        with open(self.intermediary_reference_file) as fd:
-            intermediary_input_dict = json.load(fd)
+        interm_reference_file = os.path.join(BASE_DIR,
+                                             self.intermediary_reference_file)
+
+        full_export_schedule_file = os.path.join(BASE_DIR, 'schedule_files',
+                                                 export_schedule_file)
+        with open(interm_reference_file) as fd:
+            intermediary_input_dict = jsondate.load(fd, object_hook=self._convert_struct_unicode_to_str)
 
         intermediary_input = Schedule.load_from_dict(intermediary_input_dict)
         export_output_file = tmpdir.join('exported_file')
+        export_output_filename = str(export_output_file)
 
         conv = ScheduleConverter(intermediary_input)
-        conv.export_schedule(export_output_file, handler_name)
+        conv.export_schedule(export_output_filename, handler_name)
         actual_output = export_output_file.read()
 
-        with open(export_schedule_file) as fd:
+        regenerate = os.environ.get('REGENERATE', False) == 'true'
+
+        if regenerate:
+            logger.info('test_export: Regenerating exported file from reference schedule.')
+            shutil.copy(export_output_filename, full_export_schedule_file)
+
+        with open(full_export_schedule_file) as fd:
             expected_output = fd.read()
 
         assert expected_output == actual_output
