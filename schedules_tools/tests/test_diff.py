611ae4b565e4d24dfb9a5bac2b93cfdd74ce0628
Ivo Silva <ivfernan@redhat.com>
2018-01-24 13:00:25 +0100

    add tests + test files for ScheduleDiff

--- schedules_tools/tests/test_diff.py
+++ schedules_tools/tests/test_diff.py
@@ -1,7 +1,15 @@
 import datetime
 import pytest
+import os
+import jsondate
 
 from schedules_tools.tests import create_test_schedule
+from schedules_tools.converter import ScheduleConverter
+from schedules_tools.diff_v2 import ScheduleDiff
+
+SCHEDS_DIRNAME = 'schedule_files'
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+SCHEDS_DIR = os.path.join(BASE_DIR, SCHEDS_DIRNAME)
 
 
 class TestDiff(object):
@@ -65,6 +73,42 @@ class TestDiff(object):
 
         diff = self.sch1.diff(self.sch2, whole_days=True)
         assert diff == ''
+
+
+class TestScheduleDiff(object):
+    sched_files = ['schedule-diff-a.tjx', 'schedule-diff-b.tjx']
+
+    test_data = [
+        pytest.param(('diff_output.json', sched_files), id='json'),
+        pytest.param(('diff_output.txt', sched_files), id='text')
+    ]
+
+    def import_schedule(self, filename):
+        path = os.path.join(SCHEDS_DIR, filename)
+        conv = ScheduleConverter()
+        return conv.import_schedule(path)
+
+    def diff(self, sched_a_file, sched_b_file):
+        sched_a = self.import_schedule(sched_a_file)
+        sched_b = self.import_schedule(sched_b_file)
+        return ScheduleDiff(sched_a, sched_b)
+
+    @pytest.fixture
+    def diff_results(self, request):
+        file, scheds_list = request.param
+        diff = self.diff(*scheds_list)
+
+        with open(os.path.join(BASE_DIR, 'fixtures', file)) as fd:
+            f_ext = os.path.splitext(file)[1]
+            if f_ext == '.json':
+                yield jsondate.loads(diff.dump_json()), jsondate.load(fd)
+            else:
+                yield str(diff), fd.read()
+
+    @pytest.mark.parametrize('diff_results', test_data, indirect=True)
+    def test_equal_results(self, diff_results):
+        actual, expected = diff_results
+        assert actual == expected
 
 
 class TestDiffCLI(object):
